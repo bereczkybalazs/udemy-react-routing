97acb79d3b4ba1a3c6c5abd3fc2e7c238acad0d9
Bereczky Balazs <bereczkybalazs1@gmail.com>
2018-10-02 04:11:57 -0400

    added links

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 
 import Courses from './containers/Courses/Courses';
 import Users from './containers/Users/Users';
-import {BrowserRouter, Route} from "react-router-dom";
+import {BrowserRouter, Link, Route} from "react-router-dom";
 import Tasks from "./components/Tasks/Tasks";
 
 class App extends Component {
@@ -10,7 +10,19 @@ class App extends Component {
     return (
       <BrowserRouter>
         <div className="App">
-          <Route path="/" exact component={Tasks}/>
+          <Tasks />
+          <header>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="/users">Users</Link>
+                </li>
+                <li>
+                  <Link to="/all-courses">Courses</Link>
+                </li>
+              </ul>
+            </nav>
+          </header>
           <Route path="/users" exact component={Users}/>
           <Route path="/all-courses" exact component={Courses}/>
         </div>

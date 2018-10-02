dc269573a353a64def5d13f1755a3723e3ae9321
Bereczky Balazs <bereczkybalazs1@gmail.com>
2018-10-02 04:29:27 -0400

    passing course title as a query param

--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -1,15 +1,37 @@
 import React, {Component} from 'react';
 
 class Course extends Component {
-  componentDidMount () {
-    console.log(this.props);
+
+  state = {
+    title: null,
+    id: null
+  }
+
+  componentDidMount = () => {
+    this.setState(() => {
+      return {
+        id: this.props.match.params.id
+      }
+    })
+    this.setTitle()
+  }
+
+  setTitle = () => {
+    const query = new URLSearchParams(this.props.location.search)
+    let value, type
+    for (let param of query.entries()) {
+      [type, value] = param
+    }
+    if (type === 'title' && value) {
+      this.setState({title: value})
+    }
   }
 
   render() {
     return (
       <div>
-        <h1>_COURSE_TITLE_</h1>
-        <p>You selected the Course with ID: {this.props.match.params.id}</p>
+        <h1>{this.state.title}</h1>
+        <p>You selected the Course with ID: {this.state.id}</p>
       </div>
     );
   }
